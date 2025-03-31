7024a64282788ee25637aafb2767bd05d4d67de1
Shenouda Farouk <shenouda.farouk@dlivr.io>
2025-03-31 04:54:03 +0200

    Add ESLint and Prettier configuration files; update TypeScript settings

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // enable parsing latest ECMAScript
+    sourceType: 'module', // allow use of imports
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+}; 
